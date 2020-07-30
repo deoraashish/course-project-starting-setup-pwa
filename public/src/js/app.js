f6f1a694ea143fb31cb0aacedee4a33d58cbb144
Ashish Deora <ashish.deora@greatplacetowork.com>
2020-07-30 23:56:52 +0530

    Static App Shell Caching

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -18,35 +18,3 @@ window.addEventListener('beforeinstallprompt', function(event) {
     deferredPrompt = event;
     return false;
 });
-
-// Get request by Fetch
-// fetch('https://httpbin.org/ip')
-//   .then(function(response) {
-//     console.log(response);
-//     return response.json();
-// }).then(function(data) {
-//   console.log(JSON.stringify(data));
-// }).catch(function(err) {
-//   console.log(err);
-// });
-
-// Post request by Fetch
-fetch('https://httpbin.org/post',{
-  method: 'POST',
-  headers: {
-    "Content-Type": "application/json",
-    "Accept": "application/json"
-  },
-  mode: 'no-cors',
-  body: JSON.stringify({
-    message: "Does this works?"
-  })
-}).then(function(response) {
-  console.log(response);
-  return response.json();
-}).then(function(data) {
-  console.log(JSON.stringify(data));
-}).catch(function(err) {
-  console.log(err);
-});
-
--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,24 @@
 
 self.addEventListener('install', function(event) {
     console.log('Installing ServiceWorker....', event);
+    event.waitUntil(caches.open('static').then(function(cache) {
+        console.log('Inside Caching....');
+        cache.addAll([
+            '/',
+            'index.html',
+            '/src/js/app.js',
+            '/src/js/feed.js',
+            '/src/js/fetch.js',
+            '/src/js/promise.js',
+            '/src/js/material.min.js',
+            '/src/images/main-image.jpg',
+            '/src/css/app.css',
+            '/src/css/feed.css',
+            'https://fonts.googleapis.com/css?family=Roboto:400,700',
+            'https://fonts.googleapis.com/icon?family=Material+Icons',
+            'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+        ]);
+    }));
 });
 
 // Activation happens only if all the pages of the scope of current scope are closed
@@ -10,5 +28,11 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
     // console.log('Fetching.....', event);
-    event.respondWith(fetch(event.request));
+    event.respondWith(caches.match(event.request).then(function(response) {
+        if (response) {
+            return response;
+        } else {
+            return fetch(event.request);
+        }
+    }));
 });
